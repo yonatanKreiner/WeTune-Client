c13e8fd0d147521244307933539ad1cf8120b5d2
Sol Elan <solipod8@gmail.com>
2017-07-26 00:55:05 +0300

    semek youtube

--- WebSide/scripts/controllers/main.js
+++ WebSide/scripts/controllers/main.js
@@ -16,12 +16,14 @@ angular.module('weTuneApp')
 	//vm.pin = RoomService.pin;
 	vm.src = $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + "cxG-kOTMgaA");
 
-var db2 = Database.ref("rooms/" + vm.name + "/songs").on("value", function(data){
+var db = Database.ref("rooms/" + vm.name + "/songs").on("value", function(data){
 	vm.songs = data.val();
 	if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
 		$scope.$apply();
 	}
 	console.log(vm.songs);
 });
+
+
 
   });
