b7348d7064c62b6454cc929c281605c81a9f76db
yonatan <yonatansss36@gmail.com>
2017-07-25 18:20:52 +0300

    Added firebase

--- WebSide/scripts/controllers/login.js
+++ WebSide/scripts/controllers/login.js
@@ -8,6 +8,28 @@
  * Controller of the weTuneApp
  */
 angular.module('weTuneApp')
-  .controller('LoginCtrl', function () {
-    
+  .controller('LoginCtrl', function ($scope) {
+		$scope.enterRoom = function(room) {
+			if (room.exists) {
+				$scope.loginRoom(room.name, room.pin);
+			} else {
+				$scope.createRoom(room.name, room.pin);				
+			}
+		}
+
+    $scope.createRoom = function(name, pin) {
+			if (name === 'room1' && pin === '1234') {
+				alert('create');
+			} else {
+				alert('fail');
+			}
+		}
+
+		$scope.loginRoom = function(name, pin) {
+			if (name === 'room2' && pin === '1235') {
+				alert('login');
+			} else {
+				alert('fail');
+			}
+		}
   });
---
+++ WebSide/scripts/factories/firebase.js
@@ -0,0 +1,17 @@
+angular.module('weTuneApp')
+	.factory("Auth", ["$firebaseAuth",
+		function($firebaseAuth) {
+			return $firebaseAuth();
+		}
+	])
+
+	.factory("Firebase", function() {
+		var config = {
+					apiKey: "AIzaSyC1ZOik1IDQixDMTy29Azpt9lVxmz2W7Yw",
+					authDomain: "wetune-730b4.firebaseapp.com",
+					databaseURL: "https://wetune-730b4.firebaseio.com/",
+					storageBucket: "wetune-730b4.appspot.com",
+					messagingSenderId: "440789496733",
+				};
+			return firebase.initializeApp(config);
+	});
