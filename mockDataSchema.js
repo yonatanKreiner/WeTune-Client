5c3da9be8bb52bbb3b60943e3c9b3dc1d542fbf8
yonatan <yonatansss36@gmail.com>
2017-07-25 14:17:00 +0300

    Update json schema

--- mockDataSchema.js
+++ mockDataSchema.js
@@ -13,38 +13,40 @@ export const schema = {
 						"unique": true,
 						"faker": "name.firstName"
 					},
-					 "users": {
+					"users": {
 							"type": "array",
 							"minItems": 3,
 							"maxItems": 5,
 							"items": {
 								"type": "string",
-								"faker": "internet.email"
+								"unique": true,
+								"faker": "name.firstName"
 							}
           },
           "songs": {
-           "type": "array",
-							"minItems": 3,
-							"maxItems": 5,
-							"items": {
-								"type": "object",
-								"properties": {
-									"name": {
-										"type": "string",
-										"unique": true
-									},
-									"length": {
-										"type": "integer",
-										"minimum": 1
-									},
-									"requester": {
-										"type": "string",
-										"unique": true,
-										"faker": "internet.email"
-									},
-									"required": ["name", "length", "requester"]
+						"type": "array",
+						"minItems": 3,
+						"maxItems": 5,
+						"items": {
+							"type": "object",
+							"properties": {
+								"name": {
+									"type": "string",
+									"unique": true
+								},
+								"length": {
+									"type": "integer",
+									"minimum": 1,
+									"maximum": 5
+								},
+								"requester": {
+									"type": "string",
+									"unique": true,
+									"faker": "name.firstName"
 								}
-							}
+							},
+							"required": ["name", "length", "requester"]
+						}
           }
 				},
 				"required": ["name", "users", "songs"]
