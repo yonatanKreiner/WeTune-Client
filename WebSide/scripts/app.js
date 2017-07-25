5bdb2709c8c2995001df75e7589e31d1f06c1116
Sol Elan <solipod8@gmail.com>
2017-07-25 14:33:49 +0300

    init webside

---
+++ WebSide/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name weTuneApp
+ * @description
+ * # weTuneApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('weTuneApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ WebSide/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weTuneApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weTuneApp
+ */
+angular.module('weTuneApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
