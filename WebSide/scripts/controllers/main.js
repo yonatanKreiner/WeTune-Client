13113d410d88492dc2aa864f606be19970b79871
Sol Elan <solipod8@gmail.com>
2017-07-25 18:22:51 +0300

    main page template

--- WebSide/scripts/controllers/main.js
+++ WebSide/scripts/controllers/main.js
@@ -8,6 +8,12 @@
  * Controller of the weTuneApp
  */
 angular.module('weTuneApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($sce) {
+	var vm = this;
+	vm.name = "סוליסול";
+	vm.code = "1234";
+	vm.src = $sce.trustAsResourceUrl("https://www.youtube.com/embed/ebXbLfLACGM");
 
+
+	vm.songs;
   });
