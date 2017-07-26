f9e2e5cfef7e5e24efd3940e58b5bc9930a937ce
Sol Elan <solipod8@gmail.com>
2017-07-26 04:32:57 +0300

    youtube fixed

--- WebSide/scripts/controllers/main.js
+++ WebSide/scripts/controllers/main.js
@@ -10,66 +10,46 @@
 angular.module('weTuneApp')
   .controller('MainCtrl', function ($scope, $sce, Database, RoomService) {
 	var vm = this;
-	vm.name = "tempor";
+	vm.name = "room5";
 	vm.code = "1234";
 	//vm.name = RoomService.name;
 	//vm.pin = RoomService.pin;
-//vm.src = $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + "cxG-kOTMgaA");
-	vm.src = "cxG-kOTMgaA";
+	vm.src = "XIMLoLxmTDw";
 
-Database.ref("rooms/" + vm.name + "/songs").on("value", function(data){
-	vm.songs = data.val();
-	if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
-		$scope.$apply();
-	}
-	console.log(vm.songs);
+
+
+Database.ref("rooms/" + vm.name + "/volume").on("value", function(data){
+	vm.player.setVolume(data.val());
 });
 
-  // $scope.$on('youtube.player.ended', function ($event, player) {
-
-	// 	vm.src = vm.songs[0].url;
-	// 	player.playVideo();
-  // })
-
-	var tag = document.createElement('script');
-
-	tag.src = "https://www.youtube.com/iframe_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-	var player;
-	function onYouTubeIframeAPIReady() {
-		player = new YT.Player('player', {
-			height: '390',
-			width: '640',
-			videoId: vm.src,
-			events: {
-				'onReady': onPlayerReady,
-				'onStateChange': onPlayerStateChange
-			}
-		});
-	}
-
-	function onPlayerReady(event) {
-
-		event.target.playVideo();
-	}
-
-	var done = false;
-	function onPlayerStateChange(event) {
-		if (event.data == YT.PlayerState.ENDED && !done) {
-			player.nextVideo();
-			done = true;
+Database.ref("rooms/" + vm.name + "/status").on("value", function(data){
+	if(data.val() && vm.player)
+		{
+			vm.player.playVideo();
+		}else{
+			vm.player.pauseVideo();
 		}
-	}
-	
-	function stopVideo() {
-		// status stopped
-		player.stopVideo();
-	}
+});
 
-	function setVolume(volume) {
-		player.setVolume(volume);
-	}
+$scope.$on('youtube.player.ready', function ($event, player) {
+		vm.player = player;
 
+		Database.ref("rooms/" + vm.name + "/songs").on("value", function(data){
+			vm.songs = data.val();
+			vm.src = vm.songs[Object.keys(vm.songs)[0]].url;
+			player.playVideo();
+		if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
+			$scope.$apply();
+		}
+		console.log(vm.songs);
 	});
+  })
+
+  $scope.$on('youtube.player.ended', function ($event, player) {
+		console.log(Object.keys(vm.songs)[0]);
+		Database.ref("rooms/" + vm.name + "/songs").child(Object.keys(vm.songs)[0]).remove();
+		vm.src = vm.songs[Object.keys(vm.songs)[0]].url;
+			player.playVideo();
+  })
+
+  });
