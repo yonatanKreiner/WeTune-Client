7614a546f8bce37569cefca85dae0601fade2e5f
noam sha <noam sha>
2017-07-26 06:57:12 +0300

    adding ISO time compatibility with songs length

--- WebSide/scripts/controllers/main.js
+++ WebSide/scripts/controllers/main.js
@@ -41,6 +41,9 @@ $scope.$on('youtube.player.ready', function ($event, player) {
 
 		Database.ref("rooms/" + vm.name + "/songs").on("value", function(data){
 			vm.songs = data.val();
+			vm.songs.map(function(item) {
+				item.length = $scope.convertISO8601ToSeconds(item.length) / 60;
+			});
 			vm.src = vm.songs[Object.keys(vm.songs)[0]].url;
 			player.playVideo();
 		if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
@@ -55,6 +58,22 @@ $scope.$on('youtube.player.ready', function ($event, player) {
 		Database.ref("rooms/" + vm.name + "/songs").child(Object.keys(vm.songs)[0]).remove();
 		vm.src = vm.songs[Object.keys(vm.songs)[0]].url;
 			player.playVideo();
-  })
+  });
+
+	$scope.convertISO8601ToSeconds = function(input) {
+
+		var reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
+		var hours = 0, minutes = 0, seconds = 0, totalseconds;
+
+		if (reptms.test(input)) {
+			var matches = reptms.exec(input);
+			if (matches[1]) hours = Number(matches[1]);
+			if (matches[2]) minutes = Number(matches[2]);
+			if (matches[3]) seconds = Number(matches[3]);
+			totalseconds = hours * 3600  + minutes * 60 + seconds;
+		}
+
+		return (totalseconds);
+	}
 
   });
