843fc0739466a30065e529449d2683759dff9652
yonatan kreiner <yonatanKreiner@gmail.com>
2017-08-01 19:18:06 +0300

    again

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.set('port', (process.env.PORT || 3000))
+
 app.use(express.static(__dirname + '/WebSide'))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/WebSide/index.html')
 })
 
-app.listen(3000, function () {
+app.listen(app.get('port'), function () {
   console.log('App listening on port 3000!')
 })
